8aa4ba09674557c43234af0d1f15fe2583bcd237
HerdisAdimarta <herdies.adimarta@gmail.com>
2021-01-31 15:49:13 +0700

    Initial commit

---
+++ app/src/main/java/com/android/bluetoothdistance/MainActivity.java
@@ -0,0 +1,81 @@
+package com.android.bluetoothdistance;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static android.provider.Settings.Global.DEVICE_NAME;
+
+public class MainActivity extends Activity {
+    private ListView lstvw;
+    private ArrayAdapter aAdapter;
+    private BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.btnGet);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bAdapter==null){
+                    Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(!bAdapter.isEnabled()){
+                        startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),1);
+                        Toast.makeText(getApplicationContext(),"Bluetooth Turned ON",Toast.LENGTH_SHORT).show();
+                    }
+                    Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
+                    ArrayList list = new ArrayList();
+                    if(pairedDevices.size()>0){
+                        for(BluetoothDevice device: pairedDevices){
+                            String devicename = device.getName();
+//                            String macAddress = device.getAddress();
+//                            list.add("Name: "+devicename+"MAC Address: "+macAddress);
+                            list.add("Name: "+devicename);
+                        }
+                        lstvw = (ListView) findViewById(R.id.deviceList);
+                        aAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+                        lstvw.setAdapter(aAdapter);
+                        lstvw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                                Intent intent = new Intent(MainActivity.this, Menu2Activity.class);
+//                                startActivity(intent);
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+    final BroadcastReceiver receiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                int  rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                Toast.makeText(getApplicationContext(),"  RSSI: " + rssi + "dBm", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/android/bluetoothdistance/Menu2Activity.java
@@ -0,0 +1,24 @@
+package com.android.bluetoothdistance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Menu2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+//        Button btn = (Button)findViewById(R.id.button);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText( getApplicationContext(), "Tersambung", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+}
